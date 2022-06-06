130c5cad9bb3cc945c7c1856829d1b2edca7e16a
daniniron <daniel.san.brito@live.com>
2022-06-06 14:27:45 -0300

    updade to talk aboult class members

--- Projetos/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ Projetos/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -6,15 +6,19 @@ class Pessoa {
 
     var cpf: String = "123.123.123-89"
     private set
+    constructor()
 
+    fun pessoaInfo() = "$name e $cpf"
 }
+
+
+
 
 
 
 fun main(){
     val daniel = Pessoa()
 
-    println(daniel.name)
-    println(daniel.cpf)
+    println(daniel.pessoaInfo())
 
 }
