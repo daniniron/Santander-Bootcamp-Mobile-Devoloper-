5a18c58934dbd61ab9e9418e89065b9842b8bf76
daniniron <daniel.san.brito@live.com>
2022-06-06 14:16:39 -0300

    updade to apply the privete modifier to set function if cpf property

--- Projetos/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ Projetos/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,12 +2,19 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var name: String =" Daniel"
-    var cpf:String = "123.123.123-89"
+    private set
+
+    var cpf: String = "123.123.123-89"
+    private set
+
 }
+
+
 
 fun main(){
     val daniel = Pessoa()
 
     println(daniel.name)
     println(daniel.cpf)
+
 }
