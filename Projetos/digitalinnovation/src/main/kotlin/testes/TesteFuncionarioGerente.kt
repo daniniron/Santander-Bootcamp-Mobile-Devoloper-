42314c9601d5462571df13df5982b6e3c6285b1e
daniniron <daniel.san.brito@live.com>
2022-06-06 19:36:16 -0300

    update employed class

--- Projetos/digitalinnovation/src/main/kotlin/testes/TesteFuncionarioGerente.kt
+++ Projetos/digitalinnovation/src/main/kotlin/testes/TesteFuncionarioGerente.kt
@@ -1,11 +1,11 @@
 package testes
 
-import one.digitalinnovation.digionebank.Analista
+import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
 
 
-    val maria = Analista(nome = "Maria Neves", cpf = "098.765.5432000.32", salario = 5000.0)
+    val maria = Gerente(nome = "Maria Neves", cpf = "098.765.5432000.32", salario = 5000.0)
 
     imprimeRelatorioFuncionario.imprime(maria)
 
